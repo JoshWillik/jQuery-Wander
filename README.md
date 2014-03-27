42373875baffef2fc2c2b1e018dc7f2d43fd823d
JoshWillik <joshwillik@gmail.com>
2014-03-27 15:21:12 -0400

    Basic plugin

---
+++ README.md
@@ -0,0 +1,31 @@
+# Wandering Elements
+
+Make random HTML elements wander around the page
+
+## Getting Started
+
+Download the [production version][min] or the [development version][max].
+
+[min]: https://raw.github.com/JoshWillik/jquery-wandering-elements/master/dist/jquery.wandering-elements.min.js
+[max]: https://raw.github.com/JoshWillik/jquery-wandering-elements/master/dist/jquery.wandering-elements.js
+
+In your web page:
+
+```html
+<script src="jquery.js"></script>
+<script src="dist/wandering-elements.min.js"></script>
+<script>
+jQuery(function($) {
+  $.awesome(); // "awesome"
+});
+</script>
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Release History
+_(Nothing yet)_
---
+++ src/wander.jquery.js
@@ -0,0 +1,64 @@
+/*
+ * Wandering Elements
+ * 
+ *
+ * Copyright (c) 2014 Josh Vanderwillik
+ * Licensed under the fun license.
+ */
+
+(function ($) {
+  var win = {
+    h: $( window ).height(),
+    w: $( window ).width()
+  };
+  $( window ).on( 'resize', function(){
+    win = {
+      h: $( window ).height(),
+      w: $( window ).width()
+    };
+  });
+
+  function random( max, min ){
+    min = min || 0;
+    return Math.floor( Math.random() * (max - min) ) + min;
+  }
+
+  var boogey = function( element ){
+    $( element ).animate({
+      top: random( win.h - $( element ).height() ),
+      left: random( win.w - $( element ).width() )
+    }, random( 2000, 700 ), function(){
+      boogey( element );
+    });
+  };
+
+  // Collection method.
+  $.fn.wander = function () {
+    return this.each(function (i) {
+      var $this = $( this );
+      var initial = $this.offset();
+      $( this ).css({
+        position: 'absolute',
+        top: initial.top,
+        left: initial.left
+      });
+
+      boogey( this );
+    });
+  };
+
+  // Static method.
+  $.wander = function (options) {
+    var all = $("*");
+    var wandering = [];
+    for( var i = 0; i < 11; i++ ){
+      wandering.push( all.get( Math.floor( Math.random() * all.length ) ) );
+    }
+    $(wandering).appendTo( 'body' ).wander();
+  };
+
+  // Static method default options.
+  $.wander.options = {
+    punctuation: '.'
+  };
+}(jQuery));
